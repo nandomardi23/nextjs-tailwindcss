373adb033e7687842d1e9ec23709849b3c7ca3be
nandomardi23 <fernandomardi1@gmail.com>
2022-11-21 16:08:35 +0700

    data

---
+++ components/Button.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Button({ children }) {
+    return <div className=" flex items-center justify-center ">{children}</div>;
+}
--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,11 @@
 import React from "react";
 
-export default function Home() {
+export default function index() {
     return (
-        <div className=" max-w-xl">
-            <div className="bg-white rounded-xl shadow-sm border  overflow-hidden">
-                <header className=" border-b px-4 py-2.5  bg-gray-50/50 flex items-center justify-between ">
-                    <div>
-                        <h1 className="font-medium text-xl">Card title</h1>
-                        <span className="text-gray-500">
-                            Mollit aute id duis culpa enim ullamco.
-                        </span>
-                    </div>
-                    <a
-                        href=""
-                        className=" bg-pink-500 hover:bg-pink-600 transition duration-300 focus:outline-none focus:ring focus:ring-pink-400 text-white px-5 py-2.5 rounded-xl font-semibold text-sm"
-                    >
-                        New Task
-                    </a>
-                </header>
-                <section className="px-4 py-2.5">
-                    Do labore laborum proident mollit sit sint ut nisi pariatur
-                    nisi. Mollit ad deserunt aliqua aliqua pariatur commodo
-                    adipisicing ad consectetur excepteur dolor. Nulla qui fugiat
-                    occaecat ut pariatur enim sunt ut laboris.
-                </section>
-                <footer className="border-t px-4 py-2.5 text-gray-500 bg-gray-50/50">
-                    Tempor dolore do est irure ullamco dolor tempor id tempor
-                    culpa officia.
-                </footer>
-            </div>
+        <div>
+            <button className=" bg-blue-600 hover:bg-blue-700 transition duration-300 focus:outline-none focus:ring focus:ring-blue-200 font-medium text-white px-6 py-2.5 rounded">
+                primary
+            </button>
         </div>
     );
 }
